f08b1ce9e8d006b7ff62fb96c1a77d0cac9e6c49
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2020-03-27 21:36:50 +0100

    :lipstick: #1 Include background picture

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,23 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <h1>Gemeinsam Helfen</h1>
-      <div className="button-bar">
-        <router-link to="/get-help">
-          <q-btn style="marginRight: 30">Ich brauche Hilfe</q-btn>
-        </router-link>
-        <router-link to="/help">
-          <q-btn>Ich möchte helfen</q-btn>
-        </router-link>
+      <div class="header-box">
+        <h1>Gemeinsam Helfen</h1>
+        <div>
+          <router-link to="/get-help">
+            <q-btn style="margin-right: 30px">Ich brauche Hilfe</q-btn>
+          </router-link>
+          <router-link to="/help">
+            <q-btn>Ich möchte helfen</q-btn>
+          </router-link>
+        </div>
       </div>
     </q-header>
 
     <q-page-container>
       <q-page class="flex flex-center">
-        <div className="StartPage">
-          <article>
-            <h2 color="secondary">Portal für Nachbarschaftshilfe in deiner Region</h2>
+        <div class="StartPage">
+            <h2>Portal für Nachbarschaftshilfe in deiner Region</h2>
             <p>
               <i>“Aus der Region - für die Region”</i> <br/>
               Ganz nach diesem Motto möchten wir Coronavirus-Nachbarschaftshilfe erleichtern und vermitteln. <br/><br/>
@@ -25,7 +26,7 @@
               das Projekt CoronaHelfer ins Leben gerufen, um gemeinsam die Ausbreitung des Virus zu verlangsamen und
               einander dort zu unterstützen, wo es möglich ist.
             </p>
-            <h2 color="secondary">Ziel und Vision der Initiative CoronaHelfer</h2>
+            <h2>Ziel und Vision der Initiative CoronaHelfer</h2>
             <ul>
               <li>CoronaHelfer schützt gefährdete Personen</li>
               <li>CoronaHelfer entlastet Menschen in systemrelevanten Berufen (Gesundheits-, Dienstleistungssystem
@@ -34,7 +35,7 @@
               <li>CoronaHelfer unterstütz generell Hilfesuchende</li>
               <li>Coronaviurs-Nachbarschaftshilfe für alle!</li>
             </ul>
-            <h2 color="secondary">Leitsätze für Coronavirus-Nachbarschaftshilfe</h2>
+            <h2>Leitsätze für Coronavirus-Nachbarschaftshilfe</h2>
             <ul>
               <li><b>C</b>ool bleiben</li>
               <li><b>O</b>rdentlich Hände waschen</li>
@@ -43,7 +44,7 @@
               <li><b>N</b>achbarschaftshilfe</li>
               <li><b>A</b>bstand halten</li>
             </ul>
-            <h2 color="secondary">Unser Hintergrund</h2>
+            <h2>Unser Hintergrund</h2>
             <p>
               Wie ist unsere Idee entstanden? Über soziale Netzwerke sind wir darauf aufmerksam geworden, wie unfassbar
               viele von euch in ihrer Umgebung Hilfe angesichts der aktuellen Situation anbieten möchten. Bei anderen
@@ -57,7 +58,6 @@
               können. Seitdem arbeiten wir gemeinsam auf Hochtouren an der Umsetzung und dem Ausbau unserer
               Vermittlungsplattform
             </p>
-          </article>
         </div>
       </q-page>
     </q-page-container>
@@ -65,12 +65,49 @@
 </template>
 
 <style lang="sass">
-  article
+  div[class="header-box"]
+    text-align: center
+    background: url('../statics/background.jpg') no-repeat
+    background-size: cover
+    padding: 50px
+    h1
+      font-size: 40px
+      text-transform: uppercase
+      margin: 0
+
+    .q-btn
+      background: white
+      border: 0
+      border-radius: 19px
+      color: $secondary
+      cursor: pointer
+      font-size: 20px
+      font-weight: 400
+      padding: 10px 50px
+      text-transform: uppercase
+
+      &:focus
+        outline: none
+
+      &.primary
+        background: $secondary
+        color: white
+
+      &.small
+        height: 30px
+        line-height: 30px
+        padding: 0 30px
+        font-size: 15px
+        font-weight: 600
+        margin-top: 15px
+
+  div[class="StartPage"]
     margin: 20px auto 0
     max-width: 800px
 
   h2
     color: $secondary
+
 </style>
 
 <script>
