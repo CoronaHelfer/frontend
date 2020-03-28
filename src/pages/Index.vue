beef8cc9681c214b8e7de3c78c2ed24c4f1efc21
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2020-03-28 11:22:47 +0100

    :lipstick: #1 Round off header

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,78 +1,83 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <div class="header-box">
-        <h4>Gemeinsam Helfen</h4>
-        <div>
-          <router-link to="/get-help">
-            <q-btn style="margin-right: 30px">Ich brauche Hilfe</q-btn>
-          </router-link>
-          <router-link to="/help">
-            <q-btn>Ich möchte helfen</q-btn>
-          </router-link>
-        </div>
+    <header>
+      <h4>Gemeinsam Helfen</h4>
+      <div>
+        <router-link to="/get-help">
+          <q-btn style="margin-right: 30px">Ich brauche Hilfe</q-btn>
+        </router-link>
+        <router-link to="/help">
+          <q-btn>Ich möchte helfen</q-btn>
+        </router-link>
       </div>
-    </q-header>
+    </header>
 
+    <body>
     <q-page-container>
       <q-page class="flex flex-center">
-        <div class="StartPage">
-            <h5>Portal für Nachbarschaftshilfe in deiner Region</h5>
-            <p>
-              <i>“Aus der Region - für die Region”</i> <br/>
-              Ganz nach diesem Motto möchten wir Coronavirus-Nachbarschaftshilfe erleichtern und vermitteln. <br/><br/>
-              Worum geht es? <br/>Die aktuelle Situation stellt die Gesellschaft vor neue Herausforderungen. Das
-              neuartige Coronavirus hat unser Zusammenleben in den vergangenen Wochen zunehmend eingeschränkt. Wir haben
-              das Projekt CoronaHelfer ins Leben gerufen, um gemeinsam die Ausbreitung des Virus zu verlangsamen und
-              einander dort zu unterstützen, wo es möglich ist.
-            </p>
-            <h5>Ziel und Vision der Initiative CoronaHelfer</h5>
-            <ul>
-              <li>CoronaHelfer schützt gefährdete Personen</li>
-              <li>CoronaHelfer entlastet Menschen in systemrelevanten Berufen (Gesundheits-, Dienstleistungssystem
-                etc.)
-              </li>
-              <li>CoronaHelfer unterstütz generell Hilfesuchende</li>
-              <li>Coronaviurs-Nachbarschaftshilfe für alle!</li>
-            </ul>
-            <h5>Leitsätze für Coronavirus-Nachbarschaftshilfe</h5>
-            <ul>
-              <li><b>C</b>ool bleiben</li>
-              <li><b>O</b>rdentlich Hände waschen</li>
-              <li><b>R</b>ücksichtsvoll bleiben</li>
-              <li><b>O</b>hne Hamsterkäufe</li>
-              <li><b>N</b>achbarschaftshilfe</li>
-              <li><b>A</b>bstand halten</li>
-            </ul>
-            <h5>Unser Hintergrund</h5>
-            <p>
-              Wie ist unsere Idee entstanden? Über soziale Netzwerke sind wir darauf aufmerksam geworden, wie unfassbar
-              viele von euch in ihrer Umgebung Hilfe angesichts der aktuellen Situation anbieten möchten. Bei anderen
-              Vermittlungsportalen fiel uns die große Anzahl an hilfsbereiten Menschen im Gegensatz zu nur wenigen
-              Anfragen auf.<br/><br/>
-              Wir haben erkannt, dass die Vermittlung von Fremden an Fremde eine große Herausforderung darstellt und
-              machten uns die Sicherheit unserer Nutzer zur Aufgabe. Auch eine verlässliche und schnelle Vermittlung
-              sowie die Einbindung bereits bestehender Offline-Arbeit vor Ort sind uns bei der
-              Coronavirus-Nachbarschaftshilfe wichtig.<br/><br/>
-              Dank des Hackathons #WirVsVirus der Bundesregierung haben wir ein tolles Team für unsere Idee gewinnen
-              können. Seitdem arbeiten wir gemeinsam auf Hochtouren an der Umsetzung und dem Ausbau unserer
-              Vermittlungsplattform
-            </p>
-        </div>
+        <article>
+          <h5>Portal für Nachbarschaftshilfe in deiner Region</h5>
+          <p>
+            <i>“Aus der Region - für die Region”</i> <br/>
+            Ganz nach diesem Motto möchten wir Coronavirus-Nachbarschaftshilfe erleichtern und vermitteln. <br/><br/>
+            Worum geht es? <br/>Die aktuelle Situation stellt die Gesellschaft vor neue Herausforderungen. Das
+            neuartige Coronavirus hat unser Zusammenleben in den vergangenen Wochen zunehmend eingeschränkt. Wir haben
+            das Projekt CoronaHelfer ins Leben gerufen, um gemeinsam die Ausbreitung des Virus zu verlangsamen und
+            einander dort zu unterstützen, wo es möglich ist.
+          </p>
+          <h5>Ziel und Vision der Initiative CoronaHelfer</h5>
+          <ul>
+            <li>CoronaHelfer schützt gefährdete Personen</li>
+            <li>CoronaHelfer entlastet Menschen in systemrelevanten Berufen (Gesundheits-, Dienstleistungssystem
+              etc.)
+            </li>
+            <li>CoronaHelfer unterstütz generell Hilfesuchende</li>
+            <li>Coronaviurs-Nachbarschaftshilfe für alle!</li>
+          </ul>
+          <h5>Leitsätze für Coronavirus-Nachbarschaftshilfe</h5>
+          <ul>
+            <li><b>C</b>ool bleiben</li>
+            <li><b>O</b>rdentlich Hände waschen</li>
+            <li><b>R</b>ücksichtsvoll bleiben</li>
+            <li><b>O</b>hne Hamsterkäufe</li>
+            <li><b>N</b>achbarschaftshilfe</li>
+            <li><b>A</b>bstand halten</li>
+          </ul>
+          <h5>Unser Hintergrund</h5>
+          <p>
+            Wie ist unsere Idee entstanden? Über soziale Netzwerke sind wir darauf aufmerksam geworden, wie unfassbar
+            viele von euch in ihrer Umgebung Hilfe angesichts der aktuellen Situation anbieten möchten. Bei anderen
+            Vermittlungsportalen fiel uns die große Anzahl an hilfsbereiten Menschen im Gegensatz zu nur wenigen
+            Anfragen auf.<br/><br/>
+            Wir haben erkannt, dass die Vermittlung von Fremden an Fremde eine große Herausforderung darstellt und
+            machten uns die Sicherheit unserer Nutzer zur Aufgabe. Auch eine verlässliche und schnelle Vermittlung
+            sowie die Einbindung bereits bestehender Offline-Arbeit vor Ort sind uns bei der
+            Coronavirus-Nachbarschaftshilfe wichtig.<br/><br/>
+            Dank des Hackathons #WirVsVirus der Bundesregierung haben wir ein tolles Team für unsere Idee gewinnen
+            können. Seitdem arbeiten wir gemeinsam auf Hochtouren an der Umsetzung und dem Ausbau unserer
+            Vermittlungsplattform
+          </p>
+        </article>
       </q-page>
     </q-page-container>
+    </body>
   </q-layout>
 </template>
 
 <style lang="sass">
-  div[class="header-box"]
+  header
     text-align: center
     background: url('../statics/background.jpg') no-repeat
     background-size: cover
     padding: 50px
+    color: white
+
+    a
+      text-decoration: none
+
     h4
       text-transform: uppercase
-      margin: 0 0 20px 0
+      margin: 0 0 30px 0
 
     .q-btn
       background: white
@@ -100,15 +105,29 @@
         font-weight: 600
         margin-top: 15px
 
-  div[class="StartPage"]
-    margin: 20px auto 0
-    max-width: 800px
+  body
+    &:before
+      background-color: white
+      border-radius: 100%
+      content: ''
+      height: 50px
+      position: absolute
+      top: -25px
+      width: 100%
 
-  h5
-    color: $secondary
-    text-transform: uppercase
-    font-weight: 600
+    background-color: white
+    // margin-top: 60px
+    // padding: 20px 0 20px
+    position: relative
 
+    article
+      margin: 20px auto 0
+      max-width: 800px
+
+      h5
+        color: $secondary
+        text-transform: uppercase
+        font-weight: 600
 </style>
 
 <script>
