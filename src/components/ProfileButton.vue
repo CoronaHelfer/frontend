6175144e38167aca70e29e2268c69fecc87d29f7
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2020-03-29 15:35:59 +0100

    :lipstick: #14 Add dropdown menu

---
+++ src/components/ProfileButton.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <q-btn
+      v-show="!$q.localStorage.getItem('auth').authenticated"
+      to="/login"
+      class="login-btn"
+      label="Login"/>
+
+    <li
+      v-show="$q.localStorage.getItem('auth').authenticated"
+      class="menu-btn"
+      v-on:click="open = !open">
+      <span>{{$q.localStorage.getItem('auth').firstname.toLocaleUpperCase()}}</span>
+      <q-icon :name="open ? 'fas fa-chevron-up' : 'fas fa-chevron-down'"></q-icon>
+      <ul :class="open ? '' : 'hidden'">
+        <li>PROFIL</li>
+        <li>
+          <router-link to="/profile/requests">MEINE ANZEIGEN</router-link>
+        </li>
+        <li onClick="logout()">ABMELDEN</li>
+      </ul>
+    </li>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+  .login-btn
+    background-color: $secondary
+    padding: 3px 20px
+    border-radius: 20px
+    font-weight: 600
+    font-size: 15px
+
+    button
+      padding: 0
+      margin: 0
+      background-color: transparent
+      color: WHITE
+      border: 0
+      font-size: 15px
+
+  .menu-btn
+    background-color: $secondary
+    padding: 6px 20px
+    border-radius: 20px
+    font-weight: 600
+    font-size: 15px
+    display: flex
+    justify-content: space-between
+    align-items: center
+    width: 200px
+    box-sizing: border-box
+    cursor: pointer
+    ul
+      z-index: 15
+      position: absolute
+      background-color: $primary
+      list-style-type: none
+      flex-direction: column
+      top: 0
+      right: 0
+      margin-top: 100px
+      margin-right: 80px
+      display: flex
+      padding: 3px 3px
+      border-radius: 20px
+      font-weight: 600
+      font-size: 15px
+      width: 200px
+      box-sizing: border-box
+      li
+        text-align: left
+        margin-left: 0
+        padding: 10px 32px
+        border-radius: 20px
+        cursor: pointer
+        a
+          text-decoration: none
+          color: white
+        &:hover
+          background-color: $secondary
+      &.hidden
+        display: none
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      open: false
+    }
+  },
+
+  methods: {
+    logout () {
+
+    }
+  }
+}
+</script>
