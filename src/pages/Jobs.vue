b1fb54b46a5b721fd1a5b317a349a0064c3d9e3c
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2020-04-18 12:15:50 +0100

    :lipstick: DE-6 Place title on banner

--- src/pages/Jobs.vue
+++ src/pages/Jobs.vue
@@ -1,22 +1,24 @@
 <template>
   <q-page>
     <div class="title">
-      <h1>{{$t('jobsHeader')}}</h1>
     </div>
-    <body>
-      <article>
-        <h2>{{$t('jobsFeDev')}}</h2>
-        <strong>{{$t('jobsQualifications')}}:</strong> Vue.js + Quasar
-        <p>{{$t('jobsLocation')}}</p>
+    <article>
+      <div>
+        <h3>Reinschauen lohnt sich:</h3>
+        <h1>Wir suchen Verstärkung</h1>
+      </div>
 
-        <h2>{{$t('jobsBeDev')}}</h2>
-        <strong>{{$t('jobsQualifications')}}:</strong> Node.js + Swagger
-        <p>{{$t('jobsLocation')}}</p>
+      <h2>{{$t('jobsFeDev')}}</h2>
+      <strong>{{$t('jobsQualifications')}}:</strong> Vue.js + Quasar
+      <p>{{$t('jobsLocation')}}</p>
 
-        <h2>{{$t('jobsContact1')}}</h2>
-        {{$t('jobsContact2')}}: <a href="mailto:info@coronahelfer.eu">info@coronahelfer.eu</a>
-      </article>
-    </body>
+      <h2>{{$t('jobsBeDev')}}</h2>
+      <strong>{{$t('jobsQualifications')}}:</strong> Node.js + Swagger
+      <p>{{$t('jobsLocation')}}</p>
+
+      <h2>{{$t('jobsContact1')}}</h2>
+      {{$t('jobsContact2')}}: <a href="mailto:info@coronahelfer.eu">info@coronahelfer.eu</a>
+    </article>
   </q-page>
 </template>
 
@@ -28,20 +30,27 @@
   background-size: 100%
   color: white
   height: 450px
-  h1
-    padding: 298px 676px 56px 404px
+
+/*body*/
+/*  background-color: white*/
+/*  position: relative*/
+/*  padding-bottom: 20px*/
+/*  min-height: calc(100vh - 340px)*/
+article
+  padding-top: 30px
+  margin: -450px auto 0
+  max-width: 800px
+  h3
+    padding-top: 220px
     color: white
-
-body
-  background-color: white
-  position: relative
-  padding-bottom: 20px
-  min-height: calc(100vh - 340px)
-
-  article
-    padding-top: 30px
-    margin: 20px auto 0
-    max-width: 800px
+    text-align: left
+  h1
+    margin-top: -20px
+    color: white
+    font-weight: bold
+    font-size: 70px
+    line-height: 96px
+    text-align: left
 
 h2
   margin-top: 40px
