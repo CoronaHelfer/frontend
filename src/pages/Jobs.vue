881c7009138c95619d139f66cb060862fdf20b43
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2020-04-18 12:44:31 +0100

    :lipstick: DE-6 Add text

--- src/pages/Jobs.vue
+++ src/pages/Jobs.vue
@@ -1,22 +1,29 @@
 <template>
   <q-page>
-    <div class="title">
-    </div>
+    <div class="title"/>
     <article>
-      <div>
-        <h3>Reinschauen lohnt sich:</h3>
-        <h1>Wir suchen Verstärkung</h1>
-      </div>
+      <h3>Reinschauen lohnt sich:</h3>
+      <h1>Wir suchen Verstärkung</h1>
 
-      <h2>{{$t('jobsFeDev')}}</h2>
-      <strong>{{$t('jobsQualifications')}}:</strong> Vue.js + Quasar
-      <p>{{$t('jobsLocation')}}</p>
+      <p>Der ideale Kandidat wird für den Entwurf, die Entwicklung, das Testen und das Debugging von reaktionsschnellen
+        Web- und Mobilanwendungen für das Unternehmen verantwortlich sein. Unter Verwendung von JavaScript und
+        Web-Framework-Anwendungen, wie Vue.Js und Quasar sollte dieser Kandidat in der Lage sein, Benutzer- und
+        Geschäftsanforderungen in funktionales Frontend-Design umzusetzen.</p>
 
-      <h2>{{$t('jobsBeDev')}}</h2>
-      <strong>{{$t('jobsQualifications')}}:</strong> Node.js + Swagger
-      <p>{{$t('jobsLocation')}}</p>
+      <h3 class="uppercase">Verantwortlichkeiten</h3>
+      <p>Entwerfen, Entwickeln und Testen von UI für mobile und Web-Anwendungen</p>
+      <p>Erstellen von wiederverwendbarem Code und Bibliotheken für die zukünftige Verwendung</p>
+      <p>Genaue Übersetzung von Benutzer- und Geschäftsanforderungen in funktionalen Frontend-Code</p>
 
-      <h2>{{$t('jobsContact1')}}</h2>
+      <h3 class="uppercase">Qualifikationen</h3>
+      <p>Äquivalent in Informatik</p>
+      <p>optimalerweise 2 Jahre Erfahrung in der Frontend-Entwicklung</p>
+      <p>Motivation zur eigenstädigen Weiterentwicklung</p>
+      <p>Vertrautheit mit Scrum/Agile Entwicklungsmethoden</p>
+      <p>Erfahrung in der Erstellung</p>
+      <p>objektorientierter Webanwendungen in JavaScript, Vue.Js Framework und dem Quasar Framework</p>
+
+      <h3 class="uppercase">Interessiert?</h3>
       {{$t('jobsContact2')}}: <a href="mailto:info@coronahelfer.eu">info@coronahelfer.eu</a>
     </article>
   </q-page>
@@ -31,19 +38,17 @@
   color: white
   height: 450px
 
-/*body*/
-/*  background-color: white*/
-/*  position: relative*/
-/*  padding-bottom: 20px*/
-/*  min-height: calc(100vh - 340px)*/
 article
-  padding-top: 30px
+  padding: 30px 0 20px 0
   margin: -450px auto 0
   max-width: 800px
-  h3
-    padding-top: 220px
+  min-height: calc(100vh - 340px)
+
+  h3:first-of-type
+    margin-top: 220px
     color: white
     text-align: left
+
   h1
     margin-top: -20px
     color: white
@@ -52,11 +57,16 @@ article
     line-height: 96px
     text-align: left
 
-h2
-  margin-top: 40px
+  p:first-of-type
+    margin-top: 75px
+
+  h3
+    margin-top: 100px
+
+  a
+    color: $secondary
 </style>
 
 <script>
-export default {
-}
+export default {}
 </script>
