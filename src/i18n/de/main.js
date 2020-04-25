6226112869b763c0f0fc64f9e7cc06529e94f3e4
David <david@dlouis.eu>
2020-04-25 13:13:37 +0200

    Added English to Mainpage

--- src/i18n/de/main.js
+++ src/i18n/de/main.js
@@ -6,5 +6,18 @@ export default {
   saveElder: 'schützt gefährdete Personen',
   relievePeople: 'entlastet Menschen in systemrelevanten Berufen',
   supportPeople: 'unterstützt Hilfesuchende',
-  communitySupport: 'Nachbarschaftshilfe für alle'
+  communitySupport: 'Nachbarschaftshilfe für alle',
+  indexHeading: 'PORTAL FÜR NACHBARSCHAFTSHILFE IN DEINER REGION',
+  indexSubHeading: '“Aus der Region - für die Region”',
+  indexSubHeadingTwo: 'Worum geht es',
+  indexInfoText: 'Die aktuelle Situation stellt die Gesellschaft vor neue Herausforderungen. Das neuartige Coronavirus hat unser Zusammenleben in den vergangenen Wochen zunehmend eingeschränkt. Wir haben das Projekt CoronaHelfer ins Leben gerufen, um gemeinsam die Ausbreitung des Virus zu verlangsamen und einander dort zu unterstützen, wo es möglich ist.',
+  goalsHeading: 'ZIEL UND VISION DER INITIATIVE CORONAHELFER',
+  guidingHeading: 'LEITSÄTZE FÜR CORONAVIRUS-NACHBARSCHAFTSHILFE',
+  guidingT1: 'Cool bleiben',
+  historyHeading: 'UNSER HINTERGRUND',
+  historyT1: 'Wie ist unsere Idee entstanden? Über soziale Netzwerke sind wir darauf\n' +
+      '        aufmerksam geworden, wie unfassbar viele von euch in ihrer Umgebung\n' +
+      '        Hilfe angesichts der aktuellen Situation anbieten möchten. Bei anderen\n' +
+      '        Vermittlungsportalen fiel uns die große Anzahl an hilfsbereiten Menschen\n' +
+      '        im Gegensatz zu nur wenigen Anfragen auf.'
 }
--- src/i18n/en-us/main.js
+++ src/i18n/en-us/main.js
@@ -6,5 +6,13 @@ export default {
   saveElder: 'protect people at risk',
   relievePeople: 'relieve people in systemically relevant professions',
   supportPeople: 'support those who needs help',
-  communitySupport: 'Neighborhood help for everyone'
+  communitySupport: 'Neighborhood help for everyone',
+  indexHeading: 'PORTAL FOR COMMUNITY SUPPORT IN YOUR REGION',
+  indexSubHeading: '“From the region – for the region”',
+  indexSubHeadingTwo: 'What’s it about?',
+  indexInfoText: 'The current situation has led to new challenges for our society. The novel coronavirus has increasingly restricted our community life in the past weeks. We brought the CoronaHelper project to life to slow the virus’ spread and help each other where possible.',
+  goalsHeading: 'GOAL AND VISION OF THE CORONAHELPER INITIATIVE',
+  guidingHeading: 'GUIDING PRINCIPLES OF CORONAVIRUS COMMUNITY SUPPORT',
+  guidingT1: 'Stay Cool',
+  historyT1: 'Where did our idea come from? Through our social networks we realized what an incredible number of you would like to help your community given the current situation. On other help-matching portals we were struck by how the large number of people willing to help contrasted with the low number of requests.'
 }
