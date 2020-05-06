07e2f23f2c8c484b42e96078369a02e2136ccdb6
David <david@dlouis.eu>
2020-05-06 12:43:41 +0200

    Missing Fields in ENG

--- src/i18n/en-us/press.js
+++ src/i18n/en-us/press.js
@@ -2,5 +2,10 @@ export default {
   pressTitle: 'Press Portal',
   pressSubtitle: 'CoronaHelfer Press Portal',
   srfTitle: 'SRF Radio',
-  srfDesc: 'Eine App für Mitmenschlichkeit - auch nach Corona'
+  srfDesc: 'Eine App für Mitmenschlichkeit - auch nach Corona',
+  contactTitle: 'Contact Information',
+  contactName1: 'David Louis',
+  contactTitle1: 'CEO/Founder',
+  contactName2: 'Celal Simsek',
+  contactTitle2: 'Head of Public Relationships'
 }
